248c5074f5201d6786e6e8b212f12c0014a5616b
jwryu765 <jhryu7658@gmail.com>
2026-06-01 17:25:48 +0900

    5일차 커밋

---
+++ unity/UnityBasicStudy/Assets/ExitTrigger.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class ExitTrigger : MonoBehaviour
+{
+    public GameObject gameOverText;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            gameOverText.SetActive(true);
+            Time.timeScale = 0f;
+        }
+    }
+}
+
