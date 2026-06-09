b082e911fc5d9756a57a1278833e4225bb7d099a
jwryu765 <jhryu7658@gmail.com>
2026-06-09 17:29:01 +0900

    데스크톱 개발 2일차

--- wpf/WpfPracticeSolution/WpfCafeKiosk/Models/OrderItem.cs
+++ wpf/WpfPracticeSolution/WpfCafeKiosk/Models/OrderItem.cs
@@ -8,11 +8,8 @@
         public string MenuName { get; set; }
         public int Price { get; set; }
         public int Count { get; set; }
-        public int TotalPrice {
-            get
-            {
-                return Price * Count;
-            }
-        }
+
+        // get { return Price * Count; }
+        public int TotalPrice { get; set; }
     }
 }
